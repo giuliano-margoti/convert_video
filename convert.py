f88a2096f1927733fb97aa14ea1b16f2eafb6f48
Giuliano Margoti <75986950+giuliano-margoti@users.noreply.github.com>
2026-05-04 13:42:32 -0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -6,7 +6,7 @@ import astropy.io.fits as fits
 from astropy.time import Time, TimeDelta
 
 inn_video = str(input("Nome do arquivo de vídeo: "))
-name_output = str(input("Nome do arquivo de saída: "))
+name_output = str(input("Nome do objeto observado: "))
 
 video = cv2.VideoCapture(inn_video)
 
@@ -23,6 +23,8 @@ while validacao.upper() not in ['S', 'N']:
         exposure_time = float(input("tempo de exposição: "))
         cycle_time = float(input("tempo de ciclo: ")) 
         
+        print('\n\nInforme o instante de início da exposição')
+        
         hora = int(input('hora: '))
         minu = int(input('minuto: '))
         segu = float(input('segundo: '))
@@ -31,6 +33,10 @@ while validacao.upper() not in ['S', 'N']:
         anoo = int(input('ano: '))
 
         start_time = Time(f"{anoo}-{mess}-{diaa}T{hora}:{minu}:{segu:.3f}", format="isot", scale="utc")
+        
+        end_time = Time(f"{anoo}-{mess}-{diaa}T{hora}:{minu}:{segu+exposure_time:.3f}", format="isot", scale="utc")
+        
+        mid_time = Time(f"{anoo}-{mess}-{diaa}T{hora}:{minu}:{segu+(exposure_time/2):.3f}", format="isot", scale="utc")
 
     elif validacao.upper() == 'N':
     
@@ -52,21 +58,31 @@ while sucess == True:
     image_sum = np.mean(image, axis=2)
     data = image_sum[::-1]
     
-    file_name = f"{name_output}_{n:06d}.fits"
-    
     hdu = fits.PrimaryHDU(data)
     
     if validacao.upper() == 'S':
     
         obs_time = start_time + TimeDelta(n * cycle_time, format="sec")
-    
-        hdu.header["EXPTIME"] = (exposure_time, "Tempo de exposicao [s]")    
-        hdu.header["CYCLE"]   = (cycle_time, "Tempo entre frames [s]")    
         hdu.header["DATE-OBS"] = (obs_time.isot, "Tempo do inicio da exposicao")
+        
+        file_name = f"{name_output}_{obs_time}_{n:06d}.fits"
+        
+        obs_time = end_time + TimeDelta(n * cycle_time, format="sec")
+        hdu.header["DATE-END"] = (obs_time.isot, "Tempo do inicio da exposicao")
+        
+        obs_time = mid_time + TimeDelta(n * cycle_time, format="sec")
+        hdu.header["DATE-MID"] = (obs_time.isot, "Tempo do inicio da exposicao")
+        
+        hdu.header["EXPTIME"] = (exposure_time, "Tempo de exposicao [s]")    
+        hdu.header["CYCLE"]   = (cycle_time, "Tempo entre frames [s]")
+        
+        hdu.header["OBJECT"]   = (name_output, "")    
     
         print(f"Salvo: {file_name} - Time {obs_time.isot}")
         
     elif validacao.upper() == 'N':
+    
+        file_name = f"{name_output}_{n:06d}.fits"
         print(f"Salvo: {file_name}")
 
     hdu.writeto(file_name, overwrite=True)
